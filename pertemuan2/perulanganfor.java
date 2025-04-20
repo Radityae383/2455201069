0ce9bda8e731b742b58158324e9862641a9623a0
Radityae383 <radityaeka383@gmail.com>
2025-04-20 18:45:53 +0700

    Delete pertemuan2 directory

--- pertemuan2/perulanganfor.java
+++
@@ -1,8 +0,0 @@
-public class perulanganfor {
-    public static void main(String[] args) {
-        // FOR
-        System.out.println("Perulangan FOR:");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("FOR ke-" + i);
-        }
-    }    }
